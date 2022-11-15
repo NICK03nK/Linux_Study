97506dbe412bb49c5ec1955d8f37f8a01aed63e0
nK_is_NICK <nicknk579008@outlook.com>
2022-11-15 09:00:50 +0800

    Learning environment variables and command line arguments

--- CmdLine_study/cmdline.c
+++ CmdLine_study/cmdline.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[], char* env[])
 {
 //// 观察argv中的内容
 //
@@ -13,40 +14,56 @@ int main(int argc, char* argv[])
 
 //// 模拟指令的选项
 //
-  if (argc != 2)
-  {
-    printf("Usage: \n\t%s [-a/-b/-c/-ab/-bc/-ac/-abc]\n", argv[0]);
-    return 1;
-  }
+//  if (argc != 2)
+//  {
+//    printf("Usage: \n\t%s [-a/-b/-c/-ab/-bc/-ac/-abc]\n", argv[0]);
+//    return 1;
+//  }
+//
+//  if (strcmp("-a", argv[1]) == 0)
+//  {
+//    printf("功能a\n");
+//  }
+//  if (strcmp("-b", argv[1]) == 0)
+//  {
+//    printf("功能b\n");
+//  }
+//  if (strcmp("-c", argv[1]) == 0)
+//  {
+//    printf("功能c\n");
+//  }
+//  if (strcmp("-ab", argv[1]) == 0)
+//  {
+//    printf("功能ab\n");
+//  }
+//  if (strcmp("-ac", argv[1]) == 0)
+//  {
+//    printf("功能ac\n");
+//  }
+//  if (strcmp("-bc", argv[1]) == 0)
+//  {
+//    printf("功能bc\n");
+//  }
+//  if (strcmp("-abc", argv[1]) == 0)
+//  {
+//    printf("功能abc\n");
+//  }
 
-  if (strcmp("-a", argv[1]) == 0)
-  {
-    printf("功能a\n");
-  }
-  if (strcmp("-b", argv[1]) == 0)
-  {
-    printf("功能b\n");
-  }
-  if (strcmp("-c", argv[1]) == 0)
-  {
-    printf("功能c\n");
-  }
-  if (strcmp("-ab", argv[1]) == 0)
-  {
-    printf("功能ab\n");
-  }
-  if (strcmp("-ac", argv[1]) == 0)
-  {
-    printf("功能ac\n");
-  }
-  if (strcmp("-bc", argv[1]) == 0)
-  {
-    printf("功能bc\n");
-  }
-  if (strcmp("-abc", argv[1]) == 0)
-  {
-    printf("功能abc\n");
-  }
 
+//// 环境变量列表env[]来查看环境变量
+//
+//  for (int i = 0; env[i]; ++i)
+//  {
+//    printf("env[%d]:%s\n", i, env[i]);
+//  }
+
+
+//// 使用extern char **environ来查看环境变量
+
+  extern char **environ;
+  for (int i = 0; environ[i]; ++i)
+  {
+    printf("%d:%s\n", i, environ[i]);
+  }
   return 0;
 }
