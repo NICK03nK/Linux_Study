9160cb249ecc912ada4146213102cd0b13b63a8c
nK_is_NICK <nicknk579008@outlook.com>
2022-11-25 21:58:49 +0800

    Updated execution of "cd" and "echo" commands

--- myShell/myshell.c
+++ myShell/myshell.c
@@ -11,6 +11,7 @@
 
 char lineCommand[NUM];
 char* myargv[OPT_NUM];
+int lastCode = 0;
 
 int main()
 {
@@ -43,9 +44,44 @@ int main()
 
     // 分割用户输入的指令，存入myargv中
     myargv[0] = strtok(lineCommand, " ");
-    // 当lineCommand中没有" "时，strtok()返回NULL，而myargv最后要以NULL结尾，所以一下实现方式可完美契合
     int i = 1;
+
+    // 若输入的指令为ls，则加上颜色选项
+    if (myargv[0] && strcmp(myargv[0], "ls") == 0)
+    {
+      myargv[i++] = (char*)"--color=auto";
+    }
+
+    // 当lineCommand中没有" "时，strtok()返回NULL，而myargv最后要以NULL结尾，所以一下实现方式可完美契合
     while(myargv[i++] = strtok(NULL, " "));
+    
+    // 如果输入的命令是cd，则不需要创建子进程，而是让父进程继续执行后续的命令 --- 内建命令
+    if (myargv[0] && strcmp(myargv[0], "cd") == 0)
+    {
+      if (myargv[1])
+      {
+        chdir(myargv[1]);
+      }
+
+      continue;
+    }
+    
+    // 如果输入的命令是echo，则也是一个内建命令，由父进程自己执行即可
+    if (myargv[0] && strcmp(myargv[0], "echo") == 0)
+    {
+      if (strcmp(myargv[1], "$?") == 0)
+      {
+        printf("%d\n", lastCode);
+        // 使用echo输出完上一个进程的退出码后要将lastCode置为0，因为echo也是一个进程 
+        lastCode = 0; 
+      }
+      else
+      {
+        printf("%s\n", myargv[1]);
+      }
+
+      continue;
+    }
 
     // 创建子进程来执行用户输入的命令
     pid_t id = fork();
@@ -60,7 +96,13 @@ int main()
     }
     
     // 父进程
-    waitpid(id, NULL, 0);
+    int status = 0;
+    pid_t ret = waitpid(id, &status, 0);
+
+    assert(ret > 0);
+    (void)ret;
+
+    lastCode = (status >> 8) & 0xFF;
   }
 
   return 0;
