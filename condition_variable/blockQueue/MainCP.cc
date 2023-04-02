cd5cd7c9366f9015c3b6786db5171b3d831d7048
nK_is_NICK <nicknk579008@outlook.com>
2023-04-02 10:09:21 +0800

    Use two block queues to connect three threads to work together

--- condition_variable/blockQueue/MainCP.cc
+++ condition_variable/blockQueue/MainCP.cc
@@ -4,114 +4,102 @@
 #include <unistd.h>
 #include "Task.hpp"
 
-const string operation = "+-*/%";
-int myMath(int x, int y, char op)
-{
-    int result = 0;
-    switch (op)
-    {
-    case '+':
-        result = x + y;
-        break;
-
-    case '-':
-        result = x - y;
-        break;
-
-    case '*':
-        result = x * y;
-        break;
-
-    case '/':
-    {
-        if (y == 0)
-        {
-            cerr << "div zero error" << endl;
-            result = -1;
-        }
-        else
-        {
-            result = x / y;
-        }
-    }
-        break;
-
-    case '%':
-    {
-        if (y == 0)
-        {
-            cerr << "mod zero error" << endl;
-            result = -1;
-        }
-        else
-        {
-            result = x % y;
-        }
-    }
-        break;
-
-    default:
-        break;
-    }
-
-    return result;
-}
-
+// 消费
 void* consume(void* arg)
 {
-    BlockQueue<Task>* bq = static_cast<BlockQueue<Task>*>(arg);
+    // 获取main函数中的bqs中的计算任务的阻塞队列对象
+    BlockQueue<CalTask>* c_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(arg))->c_bq;
+    // 获取main函数中的bqs中的保存任务的阻塞队列对象
+    BlockQueue<SaveTask>* s_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(arg))->s_bq;
 
     while (true)
     {
         // 消费活动
-        Task t;
-        bq->pop(&t);
+        // 1. 处理计算任务的阻塞队列中的计算任务
+        CalTask ct;
+        c_bq->pop(&ct);
+        string result = ct();
+        cout << "c_thread >> 完成计算任务: " << result << endl;
+        
+        // 生产活动
+        // 2. 向保存任务的阻塞队列中发送保存任务
+        SaveTask st(result, saveResult);
+        s_bq->push(st);
 
-        cout << "消费任务: " << t() << endl;
+        cout << "c_thread >> 推送保存任务..." << endl;
+    }
+
+    return nullptr;
+}
+
+// 生产
+void* produce(void* arg)
+{
+    // 获取main函数中的bqs中的计算任务的阻塞队列对象
+    BlockQueue<CalTask>* c_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(arg))->c_bq;
+
+    while (true)
+    {
+        // 生产活动
+        // 向计算任务的阻塞队列中发送计算任务
+        int x = rand() % 100 + 1;
+        int y = rand() % 10;
+        int operCode = rand() % operation.size();
+
+        CalTask ct(x, y, operation[operCode], myMath);
+        c_bq->push(ct);
+
+        cout << "p_thread >> 生产计算任务: " << ct.toTaskString() << endl;
+
         sleep(1);
     }
 
     return nullptr;
 }
 
-void* produce(void* arg)
+// 保存
+void* save(void* arg)
 {
-    BlockQueue<Task>* bq = static_cast<BlockQueue<Task>*>(arg);
+    // 获取main函数中的bqs中的保存任务的阻塞队列对象
+    BlockQueue<SaveTask>* s_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(arg))->s_bq;
 
     while (true)
     {
-        // 生产活动
-        int x = rand() % 100 + 1;
-        int y = rand() % 10;
-        int operCode = rand() % operation.size();
-
-        Task t(x, y, operation[operCode], myMath);
-        bq->push(t);
-
-        cout << "生产任务: " << t.toTaskString() << endl;
+        // 消费活动
+        // 处理保存任务的阻塞队列中的保存任务
+        SaveTask st;
+        s_bq->pop(&st);
+        st();
+        cout << "s_thread >> 完成保存任务" << endl;
     }
-
-    return nullptr;
 }
 
 int main()
 {
     srand((unsigned long)time(nullptr) ^ getpid());
 
-    // 创建一个阻塞队列对象，让消费者线程和生产者线程都能看到这个对象
-    BlockQueue<Task>* bq = new BlockQueue<Task>();
+    BlockQueues<CalTask, SaveTask> bqs;
 
-    // 创建一个消费者线程，一个生产者线程
-    pthread_t c, p;
-    pthread_create(&c, nullptr, consume, bq);
-    pthread_create(&p, nullptr, produce, bq);
+    // 创建一个计算任务的阻塞队列对象，让消费者线程和生产者线程都能看到这个对象
+    bqs.c_bq = new BlockQueue<CalTask>();
+    // 创建一个保存任务的阻塞队列对象，让消费者线程和生产者线程都能看到这个对象
+    bqs.s_bq = new BlockQueue<SaveTask>();
 
-    // 回收消费者线程和生产者线程
+    // p是纯生产者线程，s是纯消费者线程
+    // c既是消费者线程，也是生产者线程
+    pthread_t c, p, s;
+    pthread_create(&c, nullptr, consume, &bqs);
+    pthread_create(&p, nullptr, produce, &bqs);
+    pthread_create(&s, nullptr, save, &bqs);
+
+    // 回收三个线程
     pthread_join(c, nullptr);
     pthread_join(p, nullptr);
+    pthread_join(s, nullptr);
 
-    // 释放阻塞队列
-    delete bq;
+    // 释放两个阻塞队列
+    delete bqs.c_bq;
+    delete bqs.s_bq;
     
     return 0;
 }
--- condition_variable/blockQueue/Task.hpp
+++ condition_variable/blockQueue/Task.hpp
@@ -6,14 +6,68 @@
 #include <cstdio>
 using namespace std;
 
-class Task
+const string operation = "+-*/%";
+int myMath(int x, int y, char op)
+{
+    int result = 0;
+    switch (op)
+    {
+    case '+':
+        result = x + y;
+        break;
+
+    case '-':
+        result = x - y;
+        break;
+
+    case '*':
+        result = x * y;
+        break;
+
+    case '/':
+    {
+        if (y == 0)
+        {
+            cerr << "div zero error" << endl;
+            result = -1;
+        }
+        else
+        {
+            result = x / y;
+        }
+    }
+        break;
+
+    case '%':
+    {
+        if (y == 0)
+        {
+            cerr << "mod zero error" << endl;
+            result = -1;
+        }
+        else
+        {
+            result = x % y;
+        }
+    }
+        break;
+
+    default:
+        break;
+    }
+
+    return result;
+}
+
+// 计算任务类
+class CalTask
 {
     using func_t = function<int(int, int, char)>;
 public:
-    Task()
+    CalTask()
     {}
 
-    Task(int x, int y, char op, func_t callBack)
+    CalTask(int x, int y, char op, func_t callBack)
         :_x(x)
         , _y(y)
         , _op(op)
@@ -42,4 +96,43 @@ private:
     int _y;
     char _op;
     func_t _callBack;
+};
+
+void saveResult(const string& result)
+{
+    const string target = "./log.txt";
+    FILE* fp = fopen(target.c_str(), "a+");
+    if (!fp)
+    {
+        cerr << "fopen error" << endl;
+        return;
+    }
+
+    fputs(result.c_str(), fp);
+    fputs("\n", fp);
+
+    fclose(fp);
+}
+
+// 保存任务类
+class SaveTask
+{
+    using func_t = function<void(const string&)>;
+public:
+    SaveTask()
+    {}
+
+    SaveTask(const string& msg, func_t func)
+    :_msg(msg)
+    , _func(func)
+    {}
+
+    void operator()()
+    {
+        _func(_msg);
+    }
+
+private:
+    string _msg;
+    func_t _func;
 };
