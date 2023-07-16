2532d2dffc151883a989c955df2692b26334bf15
nK_is_NICK <nk03579008@gmail.com>
2023-07-16 11:13:04 +0800

    Set the file descriptor to nonblocking IO and call other functions when idle

--- Advanced_IO/util.hpp
+++ Advanced_IO/util.hpp
@@ -17,3 +17,18 @@ void setNoneBlock(int fd)
 
     fcntl(fd, F_SETFL, fl | O_NONBLOCK);
 }
+
+void Log()
+{
+    std::cout << "this is a log" << std::endl;
+}
+
+void Download()
+{
+    std::cout << "this is a download" << std::endl;
+}
+
+void Sql()
+{
+    std::cout << "this is a sql" << std::endl;
+}
